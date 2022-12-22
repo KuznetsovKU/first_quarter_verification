390dfcb5bea9c5ca0f2132968d583914cd340c24
KuznetsovKU <kuznetsov_k@outlook.com>
2022-12-22 23:44:36 +0300

    add comments in solution

--- CS_Solution/Program.cs
+++ CS_Solution/Program.cs
@@ -4,25 +4,25 @@
 
 string[] ChooseCorrectElements(string[] inputArray, int maxStringLength = 3)
 {
-    int counter = 0;
-    foreach (string element in inputArray)
+    int counter = 0;                                       // счетчик количества элементов для нового массива
+    foreach (string element in inputArray)                 // перебираем все элементы исходного массива
     {
-        if (element.Length <= maxStringLength) counter++;
+        if (element.Length <= maxStringLength) counter++;  // если длина элемента <= максимально допустимой по условию задачи, увеличиваем счетчик на 1
     }
-    string[] outputArray = new string[counter];
-    int j = 0;
-    for (int i = 0; i < inputArray.Length; i++)
+    string[] outputArray = new string[counter];            // инициализируем измененный массив с количеством элементов подсчитанным ранее
+    int j = 0;                                             // инициализируем индексацию измененного массива
+    for (int i = 0; i < inputArray.Length; i++)            // проходим циклом по всем элементам исходного массива
     {
-        if(inputArray[i].Length <= maxStringLength) 
+        if(inputArray[i].Length <= maxStringLength)        // если длина элемента <= максимально допустимой по условию задачи
         {
-            outputArray[j] = inputArray[i];
-            j++;
+            outputArray[j] = inputArray[i];                // присваиваем его значение очередному элементу измененного массива
+            j++;                                           // переходим к следующему элементу измененного массива
         }
     }
-    return outputArray;
+    return outputArray;                                    // возвращаем из метода измененный массив
 }
 
-void PrintArray(string[] arrayToPrint)
+void PrintArray(string[] arrayToPrint)  // метод вывода в консоль элементов массива
 {
     foreach (string element in arrayToPrint) System.Console.Write($"{element}  ");
     System.Console.WriteLine();
